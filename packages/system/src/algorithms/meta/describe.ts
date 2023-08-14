6335fdb28368f88787743f3e1206f06376ed9f31
ringeringeraja <joaosan177@gmail.com>
2023-08-14 11:38:46 -0300

    fixes

--- packages/system/src/algorithms/meta/describe.ts
+++ packages/system/src/algorithms/meta/describe.ts
@@ -1,4 +1,5 @@
-import { type Context, getResources, getResourceAsset, preloadDescription } from '@sonata-api/api'
+import type { Description } from '@sonata-api/types'
+import { type Context, type Collection, getResources, getResourceAsset, preloadDescription } from '@sonata-api/api'
 import { serialize } from '@sonata-api/common'
 
 type Props = {
@@ -10,19 +11,18 @@ type Props = {
 const describe = async (props: Props, context: Context): Promise<any> => {
   const resources = await getResources()
 
-  const collections = props?.collections?.length
+  const collections = (props?.collections?.length
     ? Object.entries(resources.collections).filter(([key]) => props.collections!.includes(key)).map(([, value]) => value)
-    : Object.values(resources.collections)
+    : Object.values(resources.collections)) as Array<Collection>
 
-  const descriptions = Object.fromEntries(
-    await Promise.all(collections.map(async (collection: any) => {
-      const { description: rawDescription } = await collection()
-      const description = await preloadDescription(rawDescription)
-      await getResourceAsset(description.$id, 'model')
+  const descriptions: Record<string, Description> = {}
+  for( const collection of collections ) {
+    const { description: rawDescription } = await collection()
+    const description = await preloadDescription(rawDescription)
+    await getResourceAsset(description.$id, 'model')
 
-      return [description.$id, description]
-    }))
-  )
+    descriptions[description.$id] = description
+  }
 
   const result: {
     descriptions: typeof descriptions
