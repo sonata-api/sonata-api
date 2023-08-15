e3eb2cfae8975daf6dd9373a3e7a4c6c19b1607f
ringeringeraja <joaosan177@gmail.com>
2023-08-15 20:26:46 -0300

    test

--- packages/api/@types/env.d.ts
+++ packages/api/@types/env.d.ts
@@ -1,6 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv extends Record<
+      // used by asset handling
+      | 'SONATA_API_SHALLOW_IMPORT'
+      // used by database
+      | 'MONGODB_URI'
+      // used by mailing
       | 'MAILING_SMTP_HOST'
       | 'MAILING_SMTP_PORT'
       | 'MAILING_SMTP_USERNAME'
