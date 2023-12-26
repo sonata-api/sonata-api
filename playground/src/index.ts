50cb9c2e3c900b6b5ce14c671e5b01e9d14574e5
ringeringeraja <joaosan177@gmail.com>
2023-12-25 23:50:32 -0300

    feat(playground): use contracts

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,4 +1,4 @@
-import { init, makeRouter, validateSilently, isLeft, unwrapEither } from 'sonata-api'
+import { init, makeRouter, isLeft, unwrapEither } from 'sonata-api'
 export * as collections from './collections'
 
 const router = makeRouter({
@@ -6,29 +6,15 @@ const router = makeRouter({
 })
 
 router.GET('/get-people', async (context) => {
-  const query = validateSilently(context.request.query, {
-    properties: {
-      name: {
-        type: 'string'
-      }
-    }
-  })
-
-  if( !query ) {
-    return {
-      error: true
-    }
-  }
-
   const personEither = await context.collections.person.functions.insert({
     what: {
-      name: query.name,
+      name: context.request.payload.name,
       job: 'programmer'
     },
   })
 
   if( isLeft(personEither) ) {
-    return unwrapEither(personEither)
+    return personEither
   }
 
   const person = unwrapEither(personEither)
@@ -43,7 +29,17 @@ router.GET('/get-people', async (context) => {
     }
   }
 
-  return context.collections.person.functions.getAll({})
+  return {
+    result: await context.collections.person.functions.getAll()
+  }
+}, {
+  contract: [
+    { properties: { name: { type: 'string' } } },
+    [
+      { properties: { _tag: { literal: 'Left' }, value: { type: 'object' } } },
+      { properties: { result: { type: 'array', items: { $ref: 'person' } } } }
+    ]
+  ]
 })
 
 init({}, (context) => {
