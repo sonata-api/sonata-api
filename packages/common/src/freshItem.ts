c5d0005fb01cfd55ac8d19b8d5b5fb5cc0b0c439
ringeringeraja <joaosan177@gmail.com>
2024-02-04 05:07:04 -0300

    fix(common): deep freshItem

--- packages/common/src/freshItem.ts
+++ packages/common/src/freshItem.ts
@@ -1,32 +1,38 @@
 import type { Description } from '@sonata-api/types'
 
+const freshProperties = (properties: Description['properties']): Record<string, any> => Object.entries(properties).reduce((a, [key, property]) => {
+  const value = (() => {
+    if( '$ref' in property ) {
+      return {}
+    }
+
+    if( 'properties' in property ) {
+      return freshProperties(property.properties)
+    }
+
+    if( 'type' in property ) {
+      switch( property.type ) {
+        case 'boolean': return false
+        case 'array': return []
+        case 'object': return {}
+      }
+    }
+
+    return null
+  })()
+
+  if( value === null ) {
+    return a
+  }
+
+  return {
+    ...a,
+    [key]: value,
+  }
+}, {})
+
 export const freshItem = (description: Pick<Description, 'properties' | 'freshItem'>) => {
-  const item = Object.entries(description.properties).reduce((a, [key, property]) => {
-    const value = (() => {
-      if( '$ref' in property ) {
-        return {}
-      }
-
-      if( 'type' in property ) {
-        switch( property.type ) {
-          case 'array': return []
-          case 'object': return {}
-          case 'boolean': return false
-        }
-      }
-
-      return null
-    })()
-
-    if( value === null ) {
-      return a
-    }
-
-    return {
-      ...a,
-      [key]: value,
-    }
-  }, {})
+  const item = freshProperties(description.properties)
 
   if( description.freshItem ) {
     Object.assign(item, description.freshItem)
