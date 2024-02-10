10e3dec2e04479c3952eb0f9cadd7d233b02ac30
ringeringeraja <joaosan177@gmail.com>
2024-02-10 19:30:54 -0300

    feat(api): update postinstall script

--- packages/api/src/__scripts__/postinstall.ts
+++ packages/api/src/__scripts__/postinstall.ts
@@ -10,7 +10,15 @@ declare global {
   type Collections = typeof import('./src').default extends infer Entrypoint
     ? 'options' extends keyof Entrypoint
       ? 'collections' extends keyof Entrypoint['options']
-        ? Entrypoint['options']['collections']
+        ? Entrypoint['options']['collections'] extends infer UserCollections
+          ? {
+            [P in keyof UserCollections]: UserCollections[P] extends infer Candidate
+              ? Candidate extends (...args: any[]) => infer Coll
+                ? Coll
+                : Candidate
+              : never
+          }
+          : never
         : never
       : never
     : never
